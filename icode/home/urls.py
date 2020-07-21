3bc4211af19c1e298f1d7a6f24be90686aae5a06
Piyush026 <piyushjaiswal026@gmail.com>
2020-07-21 13:30:26 +0530

    add contact

--- icode/home/urls.py
+++ icode/home/urls.py
@@ -3,6 +3,6 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('contact/', views.contact,name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('about/', views.about,name='about'),
 ]
--- icode/home/views.py
+++ icode/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , HttpResponse
-
+from .models import Contact
 # Create your views here.
 
 def index(request):
@@ -7,7 +7,14 @@ def index(request):
     return render(request,'home/home.html')
 
 def contact(request):
-    return HttpResponse("Contact")
+    if request.method == 'POST':    
+        name=request.POST['name']
+        phone=request.POST['phone']
+        email=request.POST['email']
+        msg=request.POST['content']
+        contact = Contact(name=name,phone=phone,email=email,content=msg)
+        contact.save()
+    return render(request,'home/contact.html')
 
 
 def about(request):
